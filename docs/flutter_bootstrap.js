f29abd4890d77608467aa36ac3ee011d703c403d
Kabirul Islam <kabirul.islam.co@gmail.com>
2026-05-02 15:14:46 +0530

    Fix all paths for GitHub Pages

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d3d45dcf251823c1769909cd43698d126db38d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1193803245"
+    serviceWorkerVersion: "2556713519"
   }
 });
