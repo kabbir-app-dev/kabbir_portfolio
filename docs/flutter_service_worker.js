9bc29a403e18d00e95891876769b42f663ebdaed
Kabirul Islam <kabirul.islam.co@gmail.com>
2026-05-02 12:58:15 +0530

    Deploy Flutter web app

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -61,7 +61,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "921d0eb5786f8fc9554c2fd374248dcf
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "88733c7ac448227ff96b3a7719e25622",
+"flutter_bootstrap.js": "6eb577808c5114ad560bb5cdd7f66837",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
